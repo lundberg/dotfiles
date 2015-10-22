689aa8be07d873cfff461391d2a377dc069a5e91
Jonas Lundberg <jonas@5monkeys.se>
2015-10-22 21:05:55 +0200

    [boostrap] Exclude apt.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE.txt" --exclude "brew.sh" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE.txt" --exclude "brew.sh" \
+    --exclude "apt.sh" -avh --no-perms . ~;
 	source ~/.bash_profile;
 
   # Configure tmux
