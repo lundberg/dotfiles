7f613593a68c0857b7926012162f5efe31040b55
Jonas Lundberg <jonas@5monkeys.se>
2018-09-26 10:40:26 +0200

    [bootstrap] Install nvm, node and dependencies

--- .bash_profile
+++ .bash_profile
@@ -42,3 +42,8 @@ fi;
 
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,37 +9,33 @@ function update() {
 		--exclude "README.md" --exclude "LICENSE.txt" --exclude "brew.sh" \
     --exclude "apt.sh" -avh --no-perms . ~;
 	source ~/.bash_profile;
-
-  # Configure tmux
-  if which tmux > /dev/null; then
-    tmux source-file ~/.tmux.conf
-  fi;
 }
 
 function install() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE.txt" --exclude "brew.sh" \
-    --exclude "apt.sh" -avh --no-perms . ~;
-	source ~/.bash_profile;
-
   # Configure tmux
   if which tmux > /dev/null; then
     tmux source-file ~/.tmux.conf
   fi;
+
+  # Install node and dependencies
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  cd ~/.config/nvim/
+  nvm install node
+  npm install
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	update;
+  update;
 	install;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-    if [ "$1" == "--update" -o "$1" == "-U" ]; then
-  		update;
+    if [ "$1" == "--install" -o "$1" == "-I" ]; then
+      update;
+  		install;
     else
   		update;
-  		install;
     fi;
 	fi;
 fi;
